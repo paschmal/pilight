8c6eba8d6ac4feb554ef3a55c615e35fff136b91
Patrick <peppi45@web.de>
2020-05-03 23:42:03 +0200

    updated to luajit-2.1

--- libs/pilight/lua_c/lua.h
+++ libs/pilight/lua_c/lua.h
@@ -9,9 +9,9 @@
 #ifndef _LUA_H_
 #define _LUA_H_
 
-#include <luajit-2.0/lua.h>
-#include <luajit-2.0/lualib.h>
-#include <luajit-2.0/lauxlib.h>
+#include <luajit-2.1/lua.h>
+#include <luajit-2.1/lualib.h>
+#include <luajit-2.1/lauxlib.h>
 
 // #include "ctrace.h"
 
